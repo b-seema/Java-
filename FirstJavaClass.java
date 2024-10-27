09019e7e8ab5977244080f9cab880be724addbb5
b-seema <seemabhardwaj916@gmail.com>
2024-10-28 01:08:09 +0530

    Your commit message here

---
+++ FirstJavaClass.java
@@ -0,0 +1,10 @@
+package learningJava;
+
+public class FirstJavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+		System.out.println("Its my first programme");
+	}
+
+}
