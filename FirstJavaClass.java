0df7b5f68ccb0cabd3e7540eebc9955f4dbf14ed
b-seema <seemabhardwaj916@gmail.com>
2024-11-23 14:30:31 +0530

    addition of two number

--- FirstJavaClass.java
+++ FirstJavaClass.java
@@ -5,6 +5,12 @@ public class FirstJavaClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("Its my first programme");
+		
+		int a = 1;
+		int b = 4;
+		int sum = a+b;
+		
+		System.out.println(sum);
 	}
 
 }
