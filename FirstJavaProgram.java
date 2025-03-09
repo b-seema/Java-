e354fcb03133d8bdaf45eb9eb87e2640e918723b
b-seema <seemabhardwaj916@gmail.com>
2025-03-09 21:29:37 +0530

    adding two number

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -4,7 +4,11 @@ public class FirstJavaProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-
+		int a=2;
+		int b=34;
+		int sum = a+b;
+		
+		System.out.println(sum);
 	}
 
 }
