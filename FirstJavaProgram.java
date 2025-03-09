bb77b6dfaaef5824ef7e2a67b534589188f89427
b-seema <seemabhardwaj916@gmail.com>
2025-03-09 21:53:56 +0530

    user input

--- FirstJavaProgram.java
+++ FirstJavaProgram.java
@@ -1,5 +1,7 @@
 package learningJava;
 
+import java.util.Scanner;
+
 public class FirstJavaProgram {
 
 	public static void main(String[] args) {
@@ -9,6 +11,19 @@ public class FirstJavaProgram {
 		int sum = a+b;
 		
 		System.out.println(sum);
+		System.out.println("Now perform addition by user interaction");
+		Scanner scanner = new Scanner(System.in);
+		 
+		System.out.println("Enter the digit");
+		int first_digit= scanner.nextInt();
+		int second_digit=scanner.nextInt();
+		double sum1= first_digit+ second_digit;
+		System.out.println("Here is result ="+ sum1);
+		
+		
+
+	
 	}
+	
 
 }
